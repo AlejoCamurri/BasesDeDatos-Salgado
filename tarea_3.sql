5c4208505798216700f7abcd1bb8dd669f5de0e9
AlejoCamurri <117654436+AlejoCamurri@users.noreply.github.com>
2025-09-16 11:01:44 -0300

    primer commit

---
+++ tarea_3.sql
@@ -0,0 +1,23 @@
+# 1. Obtener el id, nombre y apellido de todos los empleados.
+SELECT id, first_name, last_name FROM employees;
+
+# 2. Obtener el id y nombre de las categorias
+SELECT id, category FROM products;
+
+# 3. Obtener los datos de todos los clientes.
+SELECT * FROM customers;
+
+# 4. Mostrar en pantalla el id, nombre de contacto, pais y telefono de los clientes
+SELECT id, first_name, country_region, mobile_phone FROM customers;
+
+# 5. Obtener el id y nombre de todos los productos
+SELECT id, product_name FROM products;
+
+# 6. Mostrar las ciudades donde viven los empleados
+SELECT city FROM employees;
+
+# 7. Mostrar los paises donde estan ubicados los clientes
+SELECT country_region FROM customers;
+
+# 8. Obtener el nombre, id, ciudad y pais de los proveedores
+SELECT first_name, id, city, country_region FROM suppliers;
